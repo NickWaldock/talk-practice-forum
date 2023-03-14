06641daa0365ace0e30443415d91c701b724c84f
Nick Waldock <106591977+NickWaldock@users.noreply.github.com>
2023-03-14 10:01:33 +0000

    Adapt post model to include date of post and order rendered list as most recent first. Removed post.body from the list rendering

--- talk_practice_forum/blog/models.py
+++ talk_practice_forum/blog/models.py
@@ -1,18 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
+from datetime import datetime, date
 
 class Post(models.Model):
     title = models.CharField(max_length=250)
     subtitle = models.CharField(max_length=250, default='')
     author = models.ForeignKey(User, on_delete=models.CASCADE,)
     body = models.TextField()
+    post_date = models.DateField(auto_now_add=True)
 
 
     def __str__(self):
         return self.title + ' | ' + str(self.author)
     
-    
+
     def get_absolute_url(self):
         # return reverse('article-detail', args=(str(self.id))) 
         # # Sends user to single post view of post created
